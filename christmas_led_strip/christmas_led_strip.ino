9a082ea77dbb1974a9e306b8ed031d3684145c82
dimit <dimitri.ollivier14@gmail.com>
2023-02-13 11:20:38 +0100

    refacto

--- christmas_led_strip/christmas_led_strip.ino
+++ christmas_led_strip/christmas_led_strip.ino
@@ -7,16 +7,18 @@
 #include "FastLED.h"
 #include "ArduinoBLE.h"
 #define NUM_LEDS 144
-#define LED_PIN A3
-#define BUTTON_ON_OFF_PIN A1
-#define BUTTON_PROG_PIN A2
-#define POTENTIOMETER_BRIGTHNESS_PIN A4
-#define POTENTIOMETER_RATE_PIN A5
+#define LED_PIN A2
+#define BUTTON_ON_OFF_PIN A5
+#define BUTTON_PROG_PIN A6
+#define POTENTIOMETER_BRIGTHNESS_PIN A3
+#define POTENTIOMETER_RATE_PIN A4
 #define BLUETOOTH_DEVICE_NAME "ViveLaBiere♥️"
 
-BLEService newService("180A"); // creating the service
-BLEUnsignedCharCharacteristic randomReading("2A58", BLERead | BLENotify); // creating the Analog Value characteristic
-BLEByteCharacteristic switchChar("2A57", BLERead | BLEWrite); // creating the LED characteristic
+BLEService colorsService("180a");  // creating the service
+//BLEUnsignedCharCharacteristic randomReading("2A58", BLERead | BLENotify); // creating the readble only characteristic
+BLEByteCharacteristic redChar("2a57", BLERead | BLEWrite);    // creating the red writable characteristic
+BLEByteCharacteristic greenChar("2a58", BLERead | BLEWrite);  // creating the green writable characteristic
+BLEByteCharacteristic blueChar("2a59", BLERead | BLEWrite);   // creating the blue writable characteristic
 
 // define LEDs
 CRGB leds[NUM_LEDS];
@@ -47,7 +49,7 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
   LEDS.showColor(CRGB(0, 0, 0));
   LEDS.setBrightness(ledBrightness);
-  
+
   // init bluetooth
   initBluetooth();
 
@@ -64,20 +66,22 @@ void initBluetooth() {
 
   // setting a name that will appear when scanning for Bluetooth® devices
   BLE.setLocalName(BLUETOOTH_DEVICE_NAME);
-  BLE.setAdvertisedService(newService);
-  
-  // add characteristics to a service
-  newService.addCharacteristic(switchChar); 
-  newService.addCharacteristic(randomReading);
-  
-  // add the service
-  BLE.addService(newService);  
-  
-  // set initial value for characteristics
-  switchChar.writeValue(0); 
-  randomReading.writeValue(0);
+  BLE.setAdvertisedService(colorsService);
 
-  // start advertising the service 
+  // add characteristics to a service
+  colorsService.addCharacteristic(redChar);
+  colorsService.addCharacteristic(greenChar);
+  colorsService.addCharacteristic(blueChar);
+
+  // add the service
+  BLE.addService(colorsService);
+
+  // set initial value for characteristics
+  redChar.writeValue(255);
+  greenChar.writeValue(255);
+  blueChar.writeValue(255);
+
+  // start advertising the service
   BLE.advertise();
   Serial.println(" Bluetooth® device active, waiting for connections...");
 }
@@ -85,58 +89,80 @@ void initBluetooth() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(digitalRead(BUTTON_ON_OFF_PIN) == LOW) {
+  if (digitalRead(BUTTON_ON_OFF_PIN) == LOW) {
     isLedsActive = !isLedsActive;
     Serial.println(isLedsActive);
     delay(100);
-  }  
+  }
 
-  if(digitalRead(BUTTON_PROG_PIN) == LOW) {
+  if (digitalRead(BUTTON_PROG_PIN) == LOW) {
     progNum++;
-    
-    if(progNum == progCount + 1) {
+
+    if (progNum == progCount + 1) {
       progNum = 1;
     }
 
     Serial.print("Program number: ");
     Serial.println(progNum);
     delay(100);
-  }  
-
-  if(isLedsActive) {
-    switch (progNum) {
-  case 1:
-      firstProg();
-    break;
-  case 2:
-      secondProg();
-    break;
-  case 3:
-    thirdProg();
-    break;
-  case 4:
-    fourthProg();
-    break;
-  default:
-    // statements
-    break;
-}
-    } else {
-      reset();
-    }
-
-
-LEDS.setBrightness(analogRead(POTENTIOMETER_BRIGTHNESS_PIN) / 10);
-ledRate = (analogRead(POTENTIOMETER_RATE_PIN) * 10); 
-
-BLEDevice central = BLE.central(); // wait for a Bluetooth® Low Energy central
-
-  if (central) {  // if a central is connected to the peripheral
-  
-    Serial.print(switchChar.read());
   }
 
-  delay(2000);
+  if (isLedsActive && millis() >= 1000) {
+    switch (progNum) {
+      case 1:
+        firstProg();
+        break;
+      case 2:
+        secondProg();
+        break;
+      case 3:
+        thirdProg();
+        break;
+      case 4:
+        fourthProg();
+        break;
+      default:
+        // statements
+        break;
+    }
+  } else {
+    reset();
+  }
+
+
+  LEDS.setBrightness(analogRead(POTENTIOMETER_BRIGTHNESS_PIN) / 10);
+  ledRate = (analogRead(POTENTIOMETER_RATE_PIN) * 10);
+
+  // wait for a Bluetooth® Low Energy central
+  BLEDevice central = BLE.central();
+
+  if (central) {  // if a central is connected to the peripheral
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+      
+    while (central.connected()) {;
+      if (redChar.written()) {
+        Serial.println("red");
+        Serial.print(redChar.value());
+      }
+
+      if (greenChar.written()) {
+        Serial.println("green");
+        Serial.print(greenChar.value());
+      }
+
+      if (blueChar.written()) {
+        Serial.println("blue");
+        Serial.print(blueChar.value());
+      }
+    }
+    
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+
+  //le delay a des effets de bord sur le BLE
+  //delay(2000);
 }
 
 void reset() {
@@ -172,8 +198,8 @@ void fourthProg() {
 
 void showLeds(int startAt, int count, CRGB color) {
   reset();
-  
-  for(int n = startAt; n <+ NUM_LEDS; n= n + count) {
+
+  for (int n = startAt; n < +NUM_LEDS; n = n + count) {
     leds[n] = color;
   }
 
